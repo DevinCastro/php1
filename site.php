d834d072e45f1f880c7e182875f81f9f29a5ffed
DevinCastro <dcrocker21@gmail.com>
2020-11-07 18:43:08 -0800

    variables

--- site.php
+++ site.php
@@ -8,8 +8,20 @@
 <body>
 
     <?php
-        echo("<h1>Hello World</h1>");
+        echo "<h1>Hello World</h1>";
+        echo "<p>this is a paragraph</p>";
+        echo "<hr>";
+        echo "not html";
+        echo "<br>";
+
+        // lets make a variable with $
+
+        $name = "Devin";
+        $age = 26;
+
+        echo "My name is $name and I am $age years old"
+
     ?>
-    
+
 </body>
 </html>
